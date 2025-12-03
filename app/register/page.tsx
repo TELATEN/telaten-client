e27e1695d83492cde61078ee5af6fd4eb28218b4
fawwazmw <mahesfawwaz79@gmail.com>
2025-12-03 12:37:29 +0700

    feat: add Image component import to register page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
